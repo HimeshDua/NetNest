4cd3206ca9c6785d8747c5c3a8d9122d7d1ebe0e
HimeshDua <himeshdua22@gmail.com>
2025-07-31 00:08:48 +0500

    chai piyo

--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -8,7 +8,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AuthLayout from '@/layouts/auth-layout';
-import Layout from '@/layouts/layout';
+import Layout from '@/layouts/Layout';
 
 type RegisterForm = {
     name: string;
